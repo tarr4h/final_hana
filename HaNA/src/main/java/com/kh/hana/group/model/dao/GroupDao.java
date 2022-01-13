d92fd7d4bec4aebe58a70f1e038b999de0993390
hyungzin0309 <hyungzin0309@naver.com>
2022-01-13 19:43:24 +0900

    그룹쪽 에러 처

--- HaNA/src/main/java/com/kh/hana/group/model/dao/GroupDao.java
+++ HaNA/src/main/java/com/kh/hana/group/model/dao/GroupDao.java
@@ -1,19 +1,9 @@
 package com.kh.hana.group.model.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.kh.hana.group.model.vo.Group;
 
-@Mapper
-public class GroupDao {
-
-	@Autowired
-	private SqlSessionTemplate session;
+public interface GroupDao {
 	
-	public Group selectOneGroup(String groupId) {
-		return session.selectOne("group.selectOneGroup", groupId);
-	}
+	public Group selectOneGroup(String groupId);
 
 }
---
+++ HaNA/src/main/java/com/kh/hana/group/model/dao/GroupDaoImpl.java
@@ -0,0 +1,20 @@
+package com.kh.hana.group.model.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.hana.group.model.vo.Group;
+
+@Repository
+public class GroupDaoImpl implements GroupDao {
+
+	@Autowired
+	private SqlSession session;
+	
+	@Override
+	public Group selectOneGroup(String groupId) {
+		return null;
+	}
+
+}
