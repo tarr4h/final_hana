71d4b9bea02ebba4f232dfca9c872bef4a82dc93
jeonghyungo <jhsw2005@gmail.com>
2022-01-13 17:48:22 +0900

    jh

--- HaNA/src/main/java/com/kh/hana/group/controller/GroupController.java
+++ HaNA/src/main/java/com/kh/hana/group/controller/GroupController.java
@@ -1,9 +1,16 @@
 package com.kh.hana.group.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.hana.account.model.vo.Account;
+import com.kh.hana.group.model.service.GroupService;
+import com.kh.hana.group.model.vo.Group;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,10 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/group")
 @Slf4j
 public class GroupController {
-
-	@GetMapping("/groupDetail.do")
-	public void groupDetail(Authentication authentication) {
-		log.debug("authentication = {}", authentication);
+	
+	@Autowired
+	private GroupService groupService;
+	
+	@GetMapping("/groupPage/{id}")
+	public void groupDetail(@PathVariable String groupId) {
+		log.debug("groupId = {}", groupId);
+		Group group = groupService.selectOneGroup(groupId);
 	}
 	
 	@GetMapping("/groupList")
