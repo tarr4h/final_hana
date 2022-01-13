9772d41d367a2149719e6b8581b8e885958dd213
jeonghyungo <jhsw2005@gmail.com>
2022-01-13 17:07:35 +0900

    z

--- HaNA/src/main/java/com/kh/hana/group/controller/GroupController.java
+++ HaNA/src/main/java/com/kh/hana/group/controller/GroupController.java
@@ -1,5 +1,20 @@
 package com.kh.hana.group.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/group")
+@Slf4j
 public class GroupController {
 
+	@GetMapping("/groupDetail.do")
+	public void groupDetail(Authentication authentication) {
+		log.debug("authentication = {}", authentication);
+	}
 }
