46dafc125a25440a7b2d7cc1b14ae8aace7f671a
tarr4h <tarr4h@gmail.com>
2022-01-14 16:14:20 +0900

    member vo 수정

--- HaNA/sql/application.sql
+++ HaNA/sql/application.sql
@@ -9,5 +9,15 @@ commit;
 
 select * from test;
 
+drop table test;
+
 insert into test values(3, '홍길동');
-    
+delete from test where no = 3;
+
+select * from member;
+
+drop table member;
+
+commit;
+
+commit;
