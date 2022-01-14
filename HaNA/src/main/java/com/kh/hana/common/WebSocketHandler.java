45247098c49821af13151dbd2146abc468c022cd
jeonyeseong <wantyou94@gmail.com>
2022-01-14 16:30:03 +0900

    웹소켓 연결확인

--- HaNA/src/main/java/com/kh/hana/common/WebSocketHandler.java
+++ HaNA/src/main/java/com/kh/hana/common/WebSocketHandler.java
@@ -1,6 +1,35 @@
 package com.kh.hana.common;
 
-//websocket porm , servlet-context 추가
-public class WebSocketHandler {
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler{
+
+	//연결
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.info("afterConnectionEstablished 연결 성공 = {}", session);
+		System.out.println("asdfffffffff");
+	}
+	
+	//메세지
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		log.info("handleTextMessage = {}", message);
+	}
+	
+	//연결 끝
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {		
+		log.info("afterConnectionClosed 연결 끊김");
+	}
+
 
 }
