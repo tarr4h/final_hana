c39c474cb554c9a144caef84d947210618a50654
tarr4h <tarr4h@gmail.com>
2022-01-19 18:09:56 +0900

    controller

--- HaNA/src/main/java/com/kh/hana/member/controller/MemberController.java
+++ HaNA/src/main/java/com/kh/hana/member/controller/MemberController.java
@@ -83,17 +83,23 @@ public class MemberController {
 	}
 	
 	@GetMapping("/{accountType}")
-	public void memberView(@AuthenticationPrincipal Member member, @PathVariable String accountType) {
+	public void memberView(@AuthenticationPrincipal Member member, @PathVariable String accountType, Model model) {
+		if(accountType.equals("memberView")) {
+			String id = member.getId();
+			Member user = memberService.selectPersonality(id);
+			log.info("member = {}", user);
+			model.addAttribute("member", user);
+		}
 	}
 	
 	
-	@GetMapping("/memberView")
-	public void memberView(String id,Model model) {
-		Member member = memberService.selectPersonality(id);
-		log.debug("memberView = {}", member);
-		model.addAttribute("member", member);
-		
-	}
+	/*
+	 * @GetMapping("/memberView") public void memberView(String id, Model model) {
+	 * Member member = memberService.selectPersonality(id);
+	 * log.debug("memberView = {}", member); model.addAttribute("member", member);
+	 * 
+	 * }
+	 */
 	
 //	@GetMapping("/memberView")
 //	public void memberView() {}
