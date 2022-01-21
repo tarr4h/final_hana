ee65f902d4d9aec0b050d7ab2f391d9a85bf4e01
tarr4h <tarr4h@gmail.com>
2022-01-22 03:13:13 +0900

    반경계산 수정

--- HaNA/src/main/java/com/kh/hana/shop/controller/ShopController.java
+++ HaNA/src/main/java/com/kh/hana/shop/controller/ShopController.java
@@ -30,10 +30,6 @@ public class ShopController {
 	
 	@GetMapping("/shopList")
 	public ResponseEntity<?> selectShopList(@RequestParam String id, @RequestParam String locationX, @RequestParam String locationY) {
-		log.info("id = {}", id);
-		log.info("locationX = {}", locationX);
-		log.info("locationY = {}", locationY);
-		
 		Map<String, Object> data = new HashMap<>();
 		data.put("id", id);
 		data.put("locationX", locationX);
@@ -42,14 +38,8 @@ public class ShopController {
 		double maxX = Double.parseDouble(locationX) + 0.0927;
 		double maxY = Double.parseDouble(locationY) + 0.074;
 		
-		log.info("mX = {}", maxX);
-		log.info("mY = {}", maxY);
-		
 		String maxLocationX = Double.toString(maxX);
 		String maxLocationY = Double.toString(maxY);
-		
-		log.info("maxLocationX = {}", maxLocationX);
-		log.info("maxLocationY = {}", maxLocationY);
 		
 		data.put("maxLocationX", maxLocationX);
 		data.put("maxLocationY", maxLocationY);
--- HaNA/src/main/java/com/kh/hana/shop/model/service/ShopServiceImpl.java
+++ HaNA/src/main/java/com/kh/hana/shop/model/service/ShopServiceImpl.java
@@ -26,13 +26,12 @@ public class ShopServiceImpl implements ShopService {
 		
 		String locationX = (String)data.get("locationX");
 		String locationY = (String)data.get("locationY");
-		log.info("locationX, Y = {}, {}", locationX, locationY);
 		
 		List<Map<String, Object>> lastShopList = new ArrayList<>();
 		for(Map<String, Object> shop : shopList) {
 			String x = (String) shop.get("LOCATION_X");
 			String y = (String) shop.get("LOCATION_Y");
-			log.info("x = {}, y = {}", x, y);
+
 			boolean bool = CalculateArea.calculateArea(locationX, locationY, x, y);
 			log.debug("calTest = {}", bool);
 			
