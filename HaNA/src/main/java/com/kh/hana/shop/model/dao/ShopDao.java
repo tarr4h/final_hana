1222900792ae22935f7b7fa3ac0c96c4cfa7b638
tarr4h <tarr4h@gmail.com>
2022-01-25 21:44:01 +0900

    asdfasdfasdf

--- HaNA/src/main/java/com/kh/hana/shop/model/dao/ShopDao.java
+++ HaNA/src/main/java/com/kh/hana/shop/model/dao/ShopDao.java
@@ -3,9 +3,13 @@ package com.kh.hana.shop.model.dao;
 import java.util.List;
 import java.util.Map;
 
+import com.kh.hana.shop.model.vo.HashTag;
+
 public interface ShopDao {
 
 	List<Map<String, Object>> selectShopList(Map<String, Object> data);
+
+	int insertHashTag(HashTag hashTag);
 
 	
 
