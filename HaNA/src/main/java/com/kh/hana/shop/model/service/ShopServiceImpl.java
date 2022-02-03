5e270236053545f59fbcd9711ba6ee8c09f2bbce
BaekSiyoon <tksemf2543@naver.com>
2022-02-04 01:27:17 +0900

    ttttt

--- HaNA/src/main/java/com/kh/hana/shop/model/service/ShopServiceImpl.java
+++ HaNA/src/main/java/com/kh/hana/shop/model/service/ShopServiceImpl.java
@@ -63,25 +63,6 @@ public class ShopServiceImpl implements ShopService {
 		
 	}
 
-	/*
-	 * @Override public List<Map<String, Object>> selectShopList(Map<String, Object>
-	 * data) { List<Map<String, Object>> shopList = shopDao.selectShopList(data);
-	 * log.info("shopList = {}", shopList);
-	 * 
-	 * String locationX = (String)data.get("locationX"); String locationY =
-	 * (String)data.get("locationY");
-	 * 
-	 * List<Map<String, Object>> lastShopList = new ArrayList<>(); for(Map<String,
-	 * Object> shop : shopList) { String x = (String) shop.get("LOCATION_X"); String
-	 * y = (String) shop.get("LOCATION_Y"); log.info("serv shop = {}", shop);
-	 * boolean bool = CalculateArea.calculateArea(locationX, locationY, x, y);
-	 * log.info("calTest = {}", bool);
-	 * 
-	 * if(bool == true) { lastShopList.add(shop); } }
-	 * log.info("shopList LAsts = {}", lastShopList); log.info("listSize = {}",
-	 * lastShopList.size()); return lastShopList; }
-	 */
-	
 	@Override
 	public int insertHashTag(HashTag hashTag) {
 		HashTag tag = shopDao.searchHashTag(hashTag);
