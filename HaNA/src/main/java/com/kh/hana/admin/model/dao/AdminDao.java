3fed92f485477931e302f9ec89d812b644f41863
tarr4h <tarr4h@gmail.com>
2022-02-13 20:35:59 +0900

    d

--- HaNA/src/main/java/com/kh/hana/admin/model/dao/AdminDao.java
+++ HaNA/src/main/java/com/kh/hana/admin/model/dao/AdminDao.java
@@ -1,5 +1,10 @@
 package com.kh.hana.admin.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 public interface AdminDao {
+
+	List<Map<String, Object>> selectRestrictionList(int limit, int offset);
 
 }
--- HaNA/src/main/java/com/kh/hana/admin/model/dao/AdminDaoImpl.java
+++ HaNA/src/main/java/com/kh/hana/admin/model/dao/AdminDaoImpl.java
@@ -1,5 +1,9 @@
 package com.kh.hana.admin.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,4 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminDaoImpl implements AdminDao {
 	@Autowired
 	private SqlSession session;
+
+	@Override
+	public List<Map<String, Object>> selectRestrictionList(int limit, int offset) {
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		return session.selectList("admin.selectRestrictionList", null, rowBounds);
+	}
+	
+	
 }
